f1c3cbddd3e7d6e626b72ef08be6754678fe2b2d
Lucas Dohmen <lucas@dohmen.io>
2021-11-04 18:14:32 +0100

    First draft

---
+++ README.md
@@ -0,0 +1,3 @@
+# Groceries
+
+What if you could go to the supermarket and still use your list of groceries? CRDTs to the rescue!
---
+++ index.js
@@ -0,0 +1,71 @@
+import { fastify } from "fastify";
+import * as Automerge from "automerge";
+import * as pov from "point-of-view";
+import * as pug from "pug";
+import * as formbody from "fastify-formbody";
+import * as fastifyStatic from "fastify-static";
+import { resolve } from "path";
+import { List } from "./list.js";
+import { types } from "util";
+
+const list = new List();
+
+const app = fastify({ logger: true });
+
+app.register(pov, {
+  engine: {
+    pug,
+  },
+});
+
+app.register(formbody);
+
+app.register(fastifyStatic, {
+  root: resolve("static"),
+});
+
+app.get("/", async (request, reply) => {
+  return reply.view("templates/index.pug", {
+    todos: list.all(),
+    initial: list.backup(),
+    feedURL: "/stream",
+    addURL: "/",
+  });
+});
+
+app.post("/", async (request, reply) => {
+  list.add(request.body.text);
+  reply.redirect("/");
+});
+
+app.get("/stream", (request, reply) => {
+  reply.raw.writeHead(200, {
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+  });
+  reply.raw.write("\n");
+  let id = 0;
+
+  list.onChange((ev) => {
+    reply.raw.write(`id: ${id++}\n`);
+    reply.raw.write(`data: ${ev.change} \n\n`);
+  });
+});
+
+app.post("/foo", async (request, reply) => {
+  let changes = request.body.changes.map((change) => {
+    return new Uint8Array(Buffer.from(change, "base64"));
+  });
+  list.apply(changes);
+  reply.send("ok");
+});
+
+const start = async () => {
+  try {
+    await app.listen(3000, "0.0.0.0");
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+start();
