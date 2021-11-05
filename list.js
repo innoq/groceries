fab329c26b6bf2e80e965291e9aff5c2bec86ef2
Lucas Dohmen <lucas@dohmen.io>
2021-11-05 13:15:19 +0100

    Add eslint, editorconfig - mark some todos
    use tabs

--- list.js
+++ list.js
@@ -1,55 +1,58 @@
+/* eslint-env node */
 import * as Automerge from "automerge";
 import { EventEmitter } from "events";
 
+// TODO: universal?
 export class List {
-  constructor() {
-    this.state = Automerge.from({ todos: [] });
-    this.changes = [];
-    this.emitter = new EventEmitter();
-  }
+	// TODO: restore from backup?
+	constructor() {
+		this.state = Automerge.from({ todos: [] });
+		this.changes = [];
+		this.emitter = new EventEmitter();
+	}
 
-  add(text) {
-    const newState = Automerge.change(this.state, `Add ${text}`, (doc) => {
-      doc.todos.push({
-        id: this.id,
-        text: text,
-        done: false,
-      });
-    });
-    const changes = Automerge.getChanges(this.state, newState);
-    this.emitChanges(changes);
-    this.state = newState;
-  }
+	add(text) {
+		const newState = Automerge.change(this.state, `Add ${text}`, (doc) => {
+			doc.todos.push({
+				id: this.id,
+				text: text,
+				done: false,
+			});
+		});
+		const changes = Automerge.getChanges(this.state, newState);
+		this.emitChanges(changes);
+		this.state = newState;
+	}
 
-  all() {
-    return this.state.todos;
-  }
+	all() {
+		return this.state.todos;
+	}
 
-  apply(changes) {
-    let [newDoc, patch] = Automerge.applyChanges(this.state, changes);
-    this.state = newDoc;
-    this.emitChanges(changes);
-  }
+	apply(changes) {
+		let [newDoc] = Automerge.applyChanges(this.state, changes);
+		this.state = newDoc;
+		this.emitChanges(changes);
+	}
 
-  backup() {
-    let backup = Automerge.save(this.state);
-    return Buffer.from(backup).toString("base64");
-  }
+	backup() {
+		let backup = Automerge.save(this.state);
+		return Buffer.from(backup).toString("base64");
+	}
 
-  onChange(fn) {
-    this.changes.forEach((change) => {
-      fn({ change });
-    });
-    this.emitter.on("change", fn);
-  }
+	onChange(fn) {
+		this.changes.forEach((change) => {
+			fn({ change });
+		});
+		this.emitter.on("change", fn);
+	}
 
-  emitChanges(changes) {
-    changes = changes.map((change) => Buffer.from(change).toString("base64"));
-    this.changes = [...this.changes, ...changes];
-    changes.forEach((change) => {
-      this.emitter.emit("change", {
-        change,
-      });
-    });
-  }
+	emitChanges(changes) {
+		changes = changes.map((change) => Buffer.from(change).toString("base64"));
+		this.changes = [...this.changes, ...changes];
+		changes.forEach((change) => {
+			this.emitter.emit("change", {
+				change,
+			});
+		});
+	}
 }
--- static/app.js
+++ static/app.js
@@ -1,112 +1,114 @@
+/* eslint-env browser */
 import * as Automerge from "https://cdn.skypack.dev/automerge";
 
 class AutomergeList extends HTMLElement {
-  constructor() {
-    super();
-    this.changes = [];
-    this.online = true;
-    this.interval = setInterval(this.pushChanges.bind(this), 1000);
-  }
+	constructor() {
+		super();
+		this.changes = [];
+		this.online = true;
+		this.interval = setInterval(this.pushChanges.bind(this), 1000);
+	}
 
-  async pushChanges() {
-    if (this.changes.length > 0) {
-      try {
-        await fetch("/foo", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            changes: this.changes,
-          }),
-        });
-        this.changes = [];
-      } catch (e) {
-        console.log("failed");
-      }
-    }
-  }
+	async pushChanges() {
+		if (this.changes.length > 0) {
+			try {
+				await fetch("/foo", {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						changes: this.changes,
+					}),
+				});
+				this.changes = [];
+			} catch (e) {
+				console.log("failed");
+			}
+		}
+	}
 
-  connectedCallback() {
-    this.ul = this.querySelector("ul");
-    this.form = this.querySelector("form");
-    const initial = this.getAttribute("initial");
-    this.state = Automerge.load(decode(initial));
-    this.source = new EventSource(this.getAttribute("change-feed"));
-    this.onMessage = this.onMessage.bind(this);
-    this.source.addEventListener("message", this.onMessage);
-    this.form.addEventListener("submit", this.onSubmit.bind(this));
-    this.button = document.createElement("button");
-    this.button.innerText = "Go offline";
-    this.button.addEventListener("click", this.toggleOnline.bind(this));
-    this.appendChild(this.button);
-  }
+	connectedCallback() {
+		this.ul = this.querySelector("ul");
+		this.form = this.querySelector("form");
+		const initial = this.getAttribute("initial");
+		this.state = Automerge.load(decode(initial));
+		this.source = new EventSource(this.getAttribute("change-feed"));
+		this.onMessage = this.onMessage.bind(this);
+		this.source.addEventListener("message", this.onMessage);
+		this.form.addEventListener("submit", this.onSubmit.bind(this));
+		this.button = document.createElement("button");
+		this.button.innerText = "Go offline";
+		this.button.addEventListener("click", this.toggleOnline.bind(this));
+		this.appendChild(this.button);
+	}
 
-  renderList() {
-    this.ul.innerHTML = "";
-    for (const todo of this.state.todos) {
-      const li = document.createElement("li");
-      li.textContent = todo.text;
-      this.ul.appendChild(li);
-    }
-  }
+	renderList() {
+		this.ul.innerHTML = "";
+		for (const todo of this.state.todos) {
+			const li = document.createElement("li");
+			li.textContent = todo.text;
+			this.ul.appendChild(li);
+		}
+	}
 
-  change(fn) {
-    const newState = Automerge.change(this.state, fn);
-    const changes = Automerge.getChanges(this.state, newState).map(encode);
-    this.changes = [...this.changes, ...changes];
-    this.state = newState;
-    this.renderList();
-  }
+	change(fn) {
+		const newState = Automerge.change(this.state, fn);
+		const changes = Automerge.getChanges(this.state, newState).map(encode);
+		this.changes = [...this.changes, ...changes];
+		this.state = newState;
+		this.renderList();
+	}
 
-  onMessage(ev) {
-    const change = decode(ev.data);
-    let [newDoc, patch] = Automerge.applyChanges(this.state, [change]);
-    this.state = newDoc;
-    this.renderList();
-  }
+	onMessage(ev) {
+		const change = decode(ev.data);
+		let [newDoc] = Automerge.applyChanges(this.state, [change]);
+		this.state = newDoc;
+		this.renderList();
+	}
 
-  onSubmit(ev) {
-    const text = this.form.querySelector("[name=text]").value;
-    this.form.querySelector("[name=text]").value = "";
-    this.change((doc) => {
-      doc.todos.push({
-        text,
-        done: false,
-      });
-    });
-    ev.preventDefault();
-  }
+	onSubmit(ev) {
+		const text = this.form.querySelector("[name=text]").value;
+		this.form.querySelector("[name=text]").value = "";
+		this.change((doc) => {
+			doc.todos.push({
+				text,
+				done: false,
+			});
+		});
+		ev.preventDefault();
+	}
 
-  toggleOnline() {
-    if (this.online) {
-      clearInterval(this.interval);
-      this.source.removeEventListener("message", this.onMessage);
-      this.source.close();
-      this.button.innerText = "Go online";
-    } else {
-      this.interval = setInterval(this.pushChanges.bind(this), 1000);
-      this.source = new EventSource(this.getAttribute("change-feed"));
-      this.source.addEventListener("message", this.onMessage);
-      this.button.innerText = "Go offline";
-    }
-    this.online = !this.online;
-  }
+	toggleOnline() {
+		if (this.online) {
+			clearInterval(this.interval);
+			this.source.removeEventListener("message", this.onMessage);
+			this.source.close();
+			this.button.innerText = "Go online";
+		} else {
+			this.interval = setInterval(this.pushChanges.bind(this), 1000);
+			this.source = new EventSource(this.getAttribute("change-feed"));
+			this.source.addEventListener("message", this.onMessage);
+			this.button.innerText = "Go offline";
+		}
+		this.online = !this.online;
+	}
 }
 
 customElements.define("automerge-list", AutomergeList);
 
+// TODO: Can we just use TextEncoder and TextDecoder on both sites?
 // decode a base64 encoded string to a Uint8Array
 function decode(base64) {
-  let binary = window.atob(base64);
-  let len = binary.length;
-  let bytes = new Uint8Array(len);
-  for (let i = 0; i < len; i++) {
-    bytes[i] = binary.charCodeAt(i);
-  }
-  return bytes;
+	let binary = window.atob(base64);
+	let len = binary.length;
+	let bytes = new Uint8Array(len);
+	for (let i = 0; i < len; i++) {
+		bytes[i] = binary.charCodeAt(i);
+	}
+	return bytes;
 }
 
 function encode(uint) {
-  return window.btoa(String.fromCharCode.apply(null, uint));
+	return window.btoa(String.fromCharCode.apply(null, uint));
 }
